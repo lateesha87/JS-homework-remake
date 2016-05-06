4bf4ab5726f9c095de5d0faf0ff2085f094ccd6e
OIC <oic@OICs-MacBook-Air.local>
2016-05-06 08:38:25 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// var favortive animal = "dog";
+// var num = 7
+// var dog Noise =" bark bark";
+
+// alert(dog Noise);
+// console.log(dog Noise);
+// document.write(Noise);
+
+alert("dog Noise");
+console.log("dog Noise")
+document.write('Noise')
+
+var favortiveAnimal = "dog";
+var num = 7
+var dogNoise = "bark bark";
